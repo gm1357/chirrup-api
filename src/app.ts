ff41da88480eb8ed7737245248f3a2a931911500
Gabriel Machado <g_machado1@hotmail.com>
2022-01-19 22:55:37 -0300

    Added prisma configurations and simple users crud

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response } from 'express';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    return res.status(200).json({
-        message: 'Hello World!'
-    });
-})
+app.use(routes);
 
 app.listen(3333);
---
+++ src/routes/users.ts
@@ -0,0 +1,51 @@
+import { Request, Response, Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient;
+
+const usersRoutes = Router();
+
+usersRoutes.get('/', async (req: Request, res: Response) => {
+    const users = await prisma.user.findMany();
+    return res.status(200).json(users);
+});
+
+usersRoutes.post('/', async (req: Request, res: Response) => {
+    const { email, username, password } = req.body;
+
+    const user = await prisma.user.create({
+        data: { email, username, password }
+    });
+    return res.status(201).json(user);
+});
+
+usersRoutes.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const users = await prisma.user.findFirst({
+        where: { id: parseInt(id) }
+    });
+    return res.status(200).json(users);
+});
+
+usersRoutes.put('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { email, username, password } = req.body;
+
+    const users = await prisma.user.update({
+        where: { id: parseInt(id) },
+        data: { email, username, password }
+    });
+    return res.status(200).json(users);
+});
+
+usersRoutes.delete('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    await prisma.user.delete({
+        where: { id: parseInt(id) }
+    });
+    return res.status(204).send();
+});
+
+export { usersRoutes }
